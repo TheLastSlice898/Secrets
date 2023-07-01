7163929998558e4330c24a9c3188b13f5dc91fcd
TheLastSlice898 <56330886+TheLastSlice898@users.noreply.github.com>
2023-07-02 01:48:23 +1000

    Made a Waypoint system for Temple run
    Will be adding comments soon as its 1:46 am tomorrow il be sure to add them.
    
    I will be working on as well a win and lose condition for the parkore minigame since the movement is done

--- Secreets/Assets/Scripts/Run.cs
+++ Secreets/Assets/Scripts/Run.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Run : MonoBehaviour
 {
+    [SerializeField] private Waypoint wayPoints;
+
+    public float MoveSpeed;
+
+
+    private Transform currentWaypoint;
 
     public float RunSpeed;
     public bool HitWall;
-    public int RunDistance;
+    public float DistanceThreshhold;
     public float PlayerInput;
     public float Sensitivity;
-    public GameObject Camera; 
+    public GameObject Camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentWaypoint = wayPoints.GetNextWaypoint(currentWaypoint);
+        transform.position = currentWaypoint.position;
+
+        currentWaypoint = wayPoints.GetNextWaypoint(currentWaypoint);
     }
 
 
@@ -24,14 +33,26 @@ public class Run : MonoBehaviour
     }
     public void TurnRight()
     {
-    transform.rotation = transform.rotation * Quaternion.Euler(0f,90f,0f);
+        transform.rotation = transform.rotation * Quaternion.Euler(0f,90f,0f);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+
         float VerticalInput = Input.GetAxis("Vertical");
-        float RunForward = (VerticalInput * 0.05f) * Sensitivity;
+
+
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, MoveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, currentWaypoint.position) < DistanceThreshhold)
+        {
+            currentWaypoint = wayPoints.GetNextWaypoint(currentWaypoint);
+            transform.LookAt(currentWaypoint);
+        }
+
+
 
         if (HitWall == true)
         {
@@ -46,7 +67,6 @@ public class Run : MonoBehaviour
         {
            TurnRight();
         }
-        transform.position = transform.position + (transform.forward * RunForward);
     }
 
 
